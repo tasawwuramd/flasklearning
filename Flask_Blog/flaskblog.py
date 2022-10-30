1d7aee0c1a778f5ada7444e2d2ab94b6d4d201eb
tasawwuramd <tasawwuramd@gmail.com>
2022-10-30 11:36:59 +0530

    Other information added

--- Flask_Blog/flaskblog.py
+++ Flask_Blog/flaskblog.py
@@ -17,7 +17,7 @@ def about():
 # below if works only if we run from python directly if we want to run it from another module we need to use variables as mentioned below
 if __name__ == '__main__':
     # app.run()
-    app.run(debug=True)
+    app.run(debug=True,host="0.0.0.0",port=1000)
 
 # To Run in mac/linux
 # export FLASK_APP=flaskblog.py
@@ -25,3 +25,4 @@ if __name__ == '__main__':
 # set FLASK_APP=flaskblog.py
 # Run in debug mode to see live changes
 # set FLASK_DEBUG=1
+# flask run -p 5001
